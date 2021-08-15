247c398c962086c4bd3f2bcb51636365e2b43e84
kartik159 <kpant159@gmail.com>
2021-08-16 00:39:18 +0530

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";   
+
+const App = () => {
+  return (
+    <br>
+    <h1>Welcome to React Parcel Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <small>Hello This is me</small>
+     <br></br>
+    <a href="https://simple.wikipedia.org/wiki/Link">Click Here
+    </a>
+    </br>
+  );
+};
+  
+export default App;
